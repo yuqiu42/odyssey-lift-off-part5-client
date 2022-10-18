7d14dd5d2a6c3e24fea0f0a88f6e1ad3e62284b1
YQ <yuqiu42@gmail.com>
2022-10-18 15:19:05 +0800

    Update the server URI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
-import GlobalStyles from "./styles";
 import Pages from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import GlobalStyles from "./styles";
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri: "https://apollo-liftoff-server-test.herokuapp.com/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
